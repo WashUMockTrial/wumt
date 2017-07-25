58b820bf005096f448450293820834b2dd71c1f8
Alex Weil <alex@alexjweil.com>
2017-07-25 20:07:35 +0200

    Fixed scaling issues

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,2 +1,17 @@
-//= require vendor/jquery.min
-// require vendor/bootstrap.min
+// require jquery
+//= require bootstrap.min
+
+
+(function () {
+	var primaryMenu = document.getElementById('primary-menu');
+	var setMenuKind = function  () {
+		if (window.innerWidth < 768) {
+			primaryMenu.classList.remove('primary-menu');
+		} else {
+			primaryMenu.classList.add('primary-menu');
+		}
+	};
+
+	setMenuKind();
+	window.addEventListener("resize", setMenuKind);
+})();
