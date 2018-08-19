2958ae17ecab051e7c4b0abb2cd5cf82de96d15d
Alex Weil <alex@alexjweil.com>
2018-08-19 14:15:16 -0700

    Fixed year

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,17 +1,30 @@
-// require jquery
-//= require bootstrap.min
+//= require vendor/jquery.min.js
+//= require vendor/bootstrap.min.js
 
 
 (function () {
-	var primaryMenu = document.getElementById('primary-menu');
-	var setMenuKind = function  () {
+	const primaryMenu = document.getElementById('primary-menu');
+
+	function setMenuKind() {
 		if (window.innerWidth < 768) {
 			primaryMenu.classList.remove('primary-menu');
 		} else {
 			primaryMenu.classList.add('primary-menu');
 		}
-	};
+	}
 
 	setMenuKind();
 	window.addEventListener("resize", setMenuKind);
+
+	(function () {
+		const yearCounter = document.getElementById('year-counter');
+		const yearHolder = document.getElementById('year-holder');
+		// Hacky, but we'll deal for now
+		if (yearCounter === null) {
+			return;
+		}
+		const yearsOnTeam = new Date().getFullYear() - Number.parseInt(yearCounter.dataset.years);
+		yearCounter.innerText = `${yearsOnTeam}`;
+		yearHolder.innerText =  `Year${yearsOnTeam === 1 ? '' : 's'} on WUMT`;
+	})()
 })();
